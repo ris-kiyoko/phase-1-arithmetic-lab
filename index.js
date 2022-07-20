3e158fe44485396ef390ecbdf5e52b05e2ad2b59
Marisa <marisae05@gmail.com>
2022-07-19 23:31:37 -0700

    commit tree dcbdf5c7b97c010e43e65ada34e2dc7d642b6c21

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+
+let random = number + 1;
+
+
+let num3 = 5;
+let num4 = 1;
+let mod = num3 - num4;
+console.log(mod);
+
